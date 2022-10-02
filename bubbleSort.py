d2604ff8e2822122331d3921f8c947ff06d65311
Admin <brycealvir@gmail.com>
2022-10-02 08:11:21 -0700

    final revision

--- bubbleSort.py
+++ bubbleSort.py
@@ -40,7 +40,6 @@ def bubble_sort(lst):
       
   return lst
 
-
 sample_list = [1, 5, 2, 6, 7]
 print(f"Unsorted list: {sample_list}")
 bubble_sort(sample_list)
@@ -48,19 +47,4 @@ print(f"Sorted list: {sample_list}")
 
 
 
-# more efficient 
-def bubble_sort(lst):
 
-  for i in range(len(lst)):
-      swapped = False
-      print(f"iteration ")
-      for j in range(len(lst) - 1):
-          print(f"comparing {lst[j], lst[j+1]}")
-          if lst[j] > lst[j+1]:
-              lst[j], lst[j+1] = lst[j+1], lst[j]
-              swapped = True
-
-      if not swapped:
-          return
-      
-  return lst
